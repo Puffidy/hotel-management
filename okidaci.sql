2ea64e04e46e23dd188f94af335fa9db4ebde0a9
eugenb <eugenbucko01@gmail.com>
2026-01-09 22:26:05 +0100

    Update okidaci.sql

--- okidaci.sql
+++ okidaci.sql
@@ -1,4 +1,5 @@
--- ----------------------------------------------------------------------------------------------------------------------------------
+USE novi_projekt;
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 1. Nova rezervacija na istu sobu koja već rezervirana
 DELIMITER //
 
@@ -28,3 +29,72 @@ SELECT * FROM rezervacija WHERE status NOT IN ('ZAVRSENA', 'OTKAZANA');
 INSERT INTO rezervacija (gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status, napomena) 
 VALUES  				(1, 1, 7, NULL, NOW(), '2026-04-12', '2026-04-13', 1, 'POTVRDJENA', 'Test');
 -- ##########
+INSERT INTO promocija (id, naziv, kod_kupona, popust_postotak, datum_pocetka, datum_zavrsetka, aktivna) VALUES 
+(999, 'Testna Neaktivna Promocija', 'FAIL100', 50.00, '2020-01-01', '2030-12-31', 0);
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status) VALUES 
+(1, 1, 1, 999, NOW(), '2026-08-01', '2026-08-05', 2, 'POTVRDJENA');
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status)
+VALUES 
+(1, 1, 1, 1, '2026-07-15 12:00:00', '2026-08-01', '2026-08-05', 2, 'POTVRDJENA');
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 2. Provjera valjanosti promocije - ne/aktivna, vremeski period
+DELIMITER //
+
+CREATE TRIGGER trg_unos_provjera_valjanosti_promocije
+BEFORE INSERT ON rezervacija
+FOR EACH ROW
+BEGIN
+    DECLARE v_aktivna BOOLEAN;
+    DECLARE v_datum_pocetka DATE;
+    DECLARE v_datum_zavrsetka DATE;
+
+    IF NEW.promocija_id IS NOT NULL THEN
+        
+        SELECT aktivna, datum_pocetka, datum_zavrsetka 
+        INTO v_aktivna, v_datum_pocetka, v_datum_zavrsetka
+        FROM promocija 
+        WHERE id = NEW.promocija_id;
+
+        IF v_aktivna = 0 THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Greška: Odabrana promocija nije aktivna!';
+        END IF;
+
+        IF DATE(NEW.datum_rezervacije) < v_datum_pocetka OR DATE(NEW.datum_rezervacije) > v_datum_zavrsetka THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Greška: Promocija je nevažeća na odabrani datum rezervacije!';
+        END IF;
+        
+    END IF;
+END //
+
+DELIMITER ;
+-- #########
+INSERT INTO promocija (id, naziv, kod_kupona, popust_postotak, datum_pocetka, datum_zavrsetka, aktivna) VALUES 
+(999, 'Testna Neaktivna Promocija', 'FAIL100', 50.00, '2020-01-01', '2030-12-31', 0);
+
+SELECT 
+    p.id AS promocija_id, 
+    p.naziv AS naziv_promocije,
+    p.datum_pocetka AS pocetak, 
+    p.datum_zavrsetka AS kraj,
+    GROUP_CONCAT(r.id) AS popis_rezervacija_id
+FROM 
+    promocija p
+LEFT JOIN 
+    rezervacija r ON p.id = r.promocija_id
+GROUP BY 
+    p.id, p.naziv;
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status) VALUES 
+(1, 1, 1, 999, NOW(), '2026-08-01', '2026-08-05', 2, 'POTVRDJENA');
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status)
+VALUES 
+(1, 1, 1, 1, '2026-07-15 12:00:00', '2026-08-01', '2026-08-05', 2, 'POTVRDJENA');
