e4c27fe9e63f68de5dc76382ff12760bbec9acf3
eugenb <eugenbucko01@gmail.com>
2026-01-12 13:33:09 +0100

    Dodavanje preostalih okidača i upita

--- okidaci.sql
+++ okidaci.sql
@@ -268,3 +268,239 @@ SET vrijeme_check_in = '2026-07-19 14:00:01',
     status = 'U_TIJEKU'
 WHERE id = 20;
 -- ----------------------------------------------------------------------------------------------------------------------------------
+-- 5. Automatski unos svih novih rezervacija u "log_rezervacija" - - E.B.
+
+DELIMITER //
+CREATE TRIGGER trg_unos_log_rezervacija
+AFTER INSERT ON rezervacija
+FOR EACH ROW
+BEGIN
+    INSERT INTO log_rezervacije (rezervacija_id, stari_status, novi_status, korisnik_db)
+    VALUES (NEW.id, NULL, NEW.status, USER());
+END //
+DELIMITER ;
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status, napomena) 
+VALUES 
+(45, 1, 15, NULL, NOW(), '2026-11-01', '2026-11-05', 1, 'POTVRDJENA', 'Log test');
+
+UPDATE rezervacija 
+SET kraj_datum = '2026-01-12' 
+WHERE id = 1;
+
+-- 5. Upit
+SELECT 
+    l.rezervacija_id AS 'ID Rez',
+    l.id AS 'ID Log',
+    CONCAT(g.ime, ' ', g.prezime) AS 'Nositelj Rezervacije',
+    
+    DATE_FORMAT(l.vrijeme_promjene, '%d.%m.%Y %H:%i:%s') AS 'Vrijeme Promjene',
+    
+    CONCAT(
+        IFNULL(l.stari_status, 'NOVA'), 
+        '-->', 
+        l.novi_status
+    ) AS 'Tijek Promjene Statusa',
+    
+    IF(l.novi_status = 'OTKAZANA', 'Otkazana', 
+        IF(l.novi_status = 'U_TIJEKU', 'U tijeku', 
+            CONCAT(DATEDIFF(r.kraj_datum, r.pocetak_datum), ' dana')
+        )
+    ) AS 'Trajanje Boravka',
+    
+   l.korisnik_db AS 'Izvršio Korisnik'
+
+FROM 
+    log_rezervacije l
+JOIN 
+    rezervacija r ON l.rezervacija_id = r.id
+JOIN 
+    gost g ON r.gost_nositelj_id = g.id
+ORDER BY 
+    l.vrijeme_promjene DESC, l.id DESC
+    ;
+-- ----------------------------------------------------------------------------------------------------------------------------------
+-- 6. Automatski unos svih ažuriranja postojećih rezervacija u "log_rezervacija"  - - E.B.
+
+DELIMITER //
+CREATE TRIGGER trg_azuriranje_log_rezervacija
+AFTER UPDATE ON rezervacija
+FOR EACH ROW
+BEGIN
+    IF OLD.status != NEW.status THEN
+        INSERT INTO log_rezervacije (rezervacija_id, stari_status, novi_status, korisnik_db)
+        VALUES (NEW.id, OLD.status, NEW.status, USER());
+    END IF;
+END //
+DELIMITER ;
+
+UPDATE rezervacija 
+SET 
+    pocetak_datum = '2026-12-24',  
+    kraj_datum = '2026-12-30',
+    soba_id = 20,
+    napomena = 'Gost zatražio promjenu termina pa otkazao',
+    status = 'OTKAZANA'
+WHERE id = 43;
+
+-- 6. Upit
+SELECT 
+    l.rezervacija_id AS 'ID',
+    CONCAT(g.ime, ' ', g.prezime) AS 'Gost',
+    
+    CONCAT(IFNULL(l.stari_status, 'NOVA'), ' --> ', l.novi_status) AS 'Povijest Promjene Statusa',
+    DATE_FORMAT(l.vrijeme_promjene, '%H:%i:%s') AS 'Vrijeme Promjene',
+    
+    DATE_FORMAT(r.pocetak_datum, '%d.%m.%Y') AS 'Trenutni Datum Dolaska',
+    r.napomena AS 'Trenutna Napomena (Zadnja)'
+
+FROM 
+    log_rezervacije l
+JOIN 
+    rezervacija r ON l.rezervacija_id = r.id
+JOIN 
+    gost g ON r.gost_nositelj_id = g.id
+WHERE 
+    l.rezervacija_id = 43
+ORDER BY 
+    l.id DESC
+LIMIT 1;
+
+
+
+-- ---------------------------------------------------------------------------------------------------------------------------------- 
+-- 7. Automatski unos svih brisanja postojećih rezervacija u "log_rezervacija"  - - E.B.
+
+ALTER TABLE log_rezervacije DROP FOREIGN KEY fk_rezervacija_id;  -- PITAJ EUGENA
+
+DELIMITER //
+CREATE TRIGGER trg_brisanje_log_rezervacija
+AFTER DELETE ON rezervacija
+FOR EACH ROW
+BEGIN
+    INSERT INTO log_rezervacije (rezervacija_id, stari_status, novi_status, korisnik_db)
+    VALUES (OLD.id, OLD.status, 'OBRISANO', USER());
+END //
+DELIMITER ;
+
+INSERT INTO rezervacija 
+(gost_nositelj_id, zaposlenik_id, soba_id, promocija_id, datum_rezervacije, pocetak_datum, kraj_datum, broj_osoba, status, napomena) 
+VALUES 
+(10, 1, 50, NULL, NOW(),'2026-02-01','2027-02-01',1,'POTVRDJENA', 'Gost ostaje godinu dana? Typo?');
+
+
+SET SQL_SAFE_UPDATES = 1;
+
+DELETE FROM rezervacija 
+WHERE gost_nositelj_id = 10 
+  AND napomena LIKE 'Typo%';
+  
+SELECT 
+    l.id AS 'ID Log',
+    l.rezervacija_id AS 'ID Obrisane Rezervacije',
+    
+    DATE_FORMAT(l.vrijeme_promjene, '%d.%m.%Y %H:%i') AS 'Vrijeme brisanja',
+    
+    CONCAT(l.stari_status, ' --> ', l.novi_status) AS 'Status promjena',
+    
+    IFNULL(CONCAT(g.ime, ' ', g.prezime), 'Podaci trajno uklonjeni') AS 'Originalni Nositelj',
+    
+    l.korisnik_db AS 'Obrisao korisnik',
+    
+	CONCAT(TIMESTAMPDIFF(MINUTE, l.vrijeme_promjene, NOW()), ' min') AS 'Proteklo od brisanja'
+
+FROM 
+    log_rezervacije l
+
+LEFT JOIN 
+    rezervacija r ON l.rezervacija_id = r.id
+LEFT JOIN 
+    gost g ON r.gost_nositelj_id = g.id
+WHERE 
+    l.novi_status = 'OBRISANO'
+ORDER BY 
+    l.vrijeme_promjene DESC;
+
+
+
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- 8. Automatski unos besplatnog čišćenja na stavka_računu ukoliko  je boravak bio duži od tri dana  - - E.B.
+
+DROP TRIGGER trg_besplatno_ciscenje_dugi_boravak;
+
+DELIMITER //
+
+CREATE TRIGGER trg_besplatno_ciscenje_dugi_boravak
+BEFORE INSERT ON stavka_racuna
+FOR EACH ROW
+BEGIN
+    DECLARE v_naziv_usluge VARCHAR(50);
+    DECLARE v_rezervacija_id INT;
+    DECLARE v_trajanje_dana INT;
+
+    IF NEW.usluga_id IS NOT NULL THEN
+        SELECT naziv INTO v_naziv_usluge 
+        FROM usluga 
+        WHERE id = NEW.usluga_id;
+
+        IF v_naziv_usluge = 'Završno čišćenje' THEN
+            
+            SELECT rezervacija_id INTO v_rezervacija_id
+            FROM racun
+            WHERE id = NEW.racun_id;
+
+            SELECT DATEDIFF(kraj_datum, pocetak_datum) INTO v_trajanje_dana
+            FROM rezervacija
+            WHERE id = v_rezervacija_id;
+
+            IF v_trajanje_dana > 3 THEN
+                SET NEW.cijena_jedinicna = 0.00;
+
+                SET NEW.iznos_ukupno = 0.00; 
+
+                SET NEW.opis = CONCAT(NEW.opis, ' (GRATIS -Boravak duži od 3 dana)');
+            END IF;
+            
+        END IF;
+    END IF;
+END //
+DELIMITER ;
+
+
+INSERT INTO stavka_racuna (racun_id, usluga_id, tip_stavke, opis, kolicina, cijena_jedinicna, iznos_ukupno) 
+VALUES (1, 31, 'USLUGA', 'Čišćenje redovno', 1, 30.00, 30.00);
+
+INSERT INTO stavka_racuna (racun_id, usluga_id, tip_stavke, opis, kolicina, cijena_jedinicna, iznos_ukupno) 
+VALUES (7, 31, 'USLUGA', 'Čišćenje redovno', 1, 30.00, 30.00);
+
+-- 8. Upit
+SELECT 
+    CONCAT(g.ime, ' ', g.prezime) AS gost_nositelj,
+    r.id AS br_racuna,
+    DATE_FORMAT(rez.pocetak_datum, '%d.%m.%Y') AS check_in,
+    DATE_FORMAT(rez.kraj_datum, '%d.%m.%Y') AS check_out,
+    DATEDIFF(rez.kraj_datum, rez.pocetak_datum) AS trajanje_nocenja,
+    sr.opis AS stavka_opis,
+    CONCAT(FORMAT(u.cijena_trenutna, 2), ' €') AS redovna_cijena,
+    CONCAT(FORMAT(sr.cijena_jedinicna, 2), ' €') AS naplacena_cijena,
+    
+    IF(DATEDIFF(rez.kraj_datum, rez.pocetak_datum) > 3 AND sr.cijena_jedinicna = 0, 
+       'Gratis',
+       
+       IF(DATEDIFF(rez.kraj_datum, rez.pocetak_datum) <= 3 AND sr.cijena_jedinicna > 0,
+          'Naplaćeno',
+          
+          'GREŠKA: Trigger ne šljaka!'
+       )
+    ) AS status
+
+FROM stavka_racuna sr
+JOIN racun r ON sr.racun_id = r.id
+JOIN rezervacija rez ON r.rezervacija_id = rez.id
+JOIN gost g ON rez.gost_nositelj_id = g.id
+JOIN usluga u ON sr.usluga_id = u.id
+WHERE u.naziv = 'Završno čišćenje'
+ORDER BY r.id;
